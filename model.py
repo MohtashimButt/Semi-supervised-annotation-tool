542451c2db5be4aa43f729024cdd08a9b6174876
Mohtashim Butt <blablah@outlook.com>
2024-08-13 22:39:50 +0500

    Fixed transformation and resizing bug

--- model.py
+++ model.py
@@ -18,8 +18,9 @@ def get_inference(input_dir):
         os.makedirs(output_dir)
 
     transform = transforms.Compose([
-        transforms.Resize((512, 512)),
-        transforms.ToTensor()
+        transforms.Resize((520, 520)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     # Load pre-trained DeepLabv3+ model
